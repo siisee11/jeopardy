60d2bd1d0efefad8a4b063f069d4417d383dc474
Nam JaeYoun <siisee111@gmail.com>
2019-08-23 14:20:07 +0900

    update

---
+++ open_dev/open_dev.c
@@ -0,0 +1,26 @@
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/mman.h>
+
+int main(void)
+{
+	int             fd;
+	char           *mptr;
+	fd = open("/dev/pmem0", O_RDWR | O_SYNC);
+	if (fd == -1) {
+		printf("open error...\n");
+		return 1;
+	}
+	mptr = mmap(0, 1 * 1024 * 1024, PROT_READ | PROT_WRITE, MAP_FILE | MAP_SHARED, fd, 4096);
+	close(fd);
+	printf("On start, mptr points to 0x%lX.\n",(unsigned long) mptr);
+	printf("mptr points to 0x%lX. *mptr = 0x%X\n", (unsigned long) mptr, *mptr);
+	mptr[0] = 'a';
+	mptr[1] = 'b';
+	printf("mptr points to 0x%lX. *mptr = 0x%X\n", (unsigned long) mptr, *mptr);
+	printf("in dev, %c %c\n", mptr[0], mptr[1]);
+
+	return 0;
+}
