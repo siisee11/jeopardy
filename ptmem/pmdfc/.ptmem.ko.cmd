9b420fb4bebccadfefdf4140b8decfcc680d5e87
Jaeyoun <siisee111@gmail.com>
2019-11-15 22:22:05 +0900

    Upload ptmem.c

---
+++ ptmem/pmdfc/.ptmem.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/siisee11/linux-kernel/linux-4.19.80/drivers/pmdfc/ptmem.ko := ld -r -m elf_x86_64  -z max-page-size=0x200000 -T ./scripts/module-common.lds  --build-id  -o /home/siisee11/linux-kernel/linux-4.19.80/drivers/pmdfc/ptmem.ko /home/siisee11/linux-kernel/linux-4.19.80/drivers/pmdfc/ptmem.o /home/siisee11/linux-kernel/linux-4.19.80/drivers/pmdfc/ptmem.mod.o ;  true
---
+++ ptmem/pmdfc/.tmp_versions/ptmem.mod
@@ -0,0 +1,3 @@
+/home/siisee11/linux-kernel/linux-4.19.80/drivers/pmdfc/ptmem.ko
+/home/siisee11/linux-kernel/linux-4.19.80/drivers/pmdfc/ptmem.o
+
