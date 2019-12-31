cd4aba1b0b5e7f50fdda76593a8212f5f879de7d
Jaeyoun <siisee111@gmail.com>
2019-12-31 16:34:26 +0900

    Upload code for debug

---
+++ pmdfc/cat_count.sh
@@ -0,0 +1,10 @@
+#bin/bash!
+
+echo "succ_gets : "
+cat /sys/kernel/debug/cleancache/succ_gets
+echo "failed_gets : "
+cat /sys/kernel/debug/cleancache/failed_gets
+echo "puts : "
+cat /sys/kernel/debug/cleancache/puts
+echo "invalidates : "
+cat /sys/kernel/debug/cleancache/invalidates
